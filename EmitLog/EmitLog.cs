fd13628ac26c16888758864b701c2bd75ebede2a
duong6003 <87841563+duong6003@users.noreply.github.com>
2022-03-07 23:39:07 +0700

    d

---
+++ EmitLog/EmitLog.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using RabbitMQ.Client;
+using System.Text;
+
+class EmitLog
+{
+    public static void Main(string[] args)
+    {
+        var factory = new ConnectionFactory() { HostName = "localhost" };
+        using(var connection = factory.CreateConnection())
+        using(var channel = connection.CreateModel())
+        {
+            channel.ExchangeDeclare(exchange: "topic_logs", type: ExchangeType.Topic);
+
+            var severity = (args.Length > 0) ? args[0] : "topic_logs";
+            var message = (args.Length > 1)
+                          ? string.Join(" ", args.Skip(1).ToArray())
+                          : "Hello World!";
+            var body = Encoding.UTF8.GetBytes(message);
+            channel.BasicPublish(exchange: "topic_logs",
+                                 routingKey: severity,
+                                 basicProperties: null,
+                                 body: body);
+            Console.WriteLine(" [x] Sent '{0}':'{1}'", severity, message);
+        }
+
+        Console.WriteLine(" Press [enter] to exit.");
+        Console.ReadLine();
+    }
+}
